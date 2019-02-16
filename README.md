6fd88656df3a1d963ec2da2cfbdd7b2f25cefecd
MahmudulHassan5809 <mahmudul.hassan240@gmail.com>
2019-02-17 01:23:41 +0600

    Complete

---
+++ README.md
@@ -0,0 +1,19 @@
+# Text Sentiment Analyis(NLP)
+
+    Machine Learning With FLask
+
+
+## Quick Start Using Pipenv For Server
+
+``` bash
+# Install dependencies
+$ pip install -r requirements.txt
+
+```
+
+# Application Image
+
+![app](1.png?raw=true "Title")
+
+# Author
+	Mahmudul Hassan
---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template,url_for,request
+import pickle
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+# Variables
+app = Flask(__name__)
+
+# Views
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+
+# Views
+@app.route('/predict', methods=['GET', 'POST'])
+def predict():
+	if request.method == 'POST':
+		with open('data/clf.pickle','rb') as f:
+			clf = pickle.load(f)
+		with open('data/tfidfmodel.pickle','rb') as f:
+			tfidf = pickle.load(f)
+		text = request.form['text']
+		data = [text]
+		vect = tfidf.transform(data).toarray()
+		my_prediction = clf.predict(vect)
+		return render_template('home.html',predction = my_prediction)
+	return render_template('home.html')
+
+# Run
+if __name__ == '__main__':
+    app.run(debug=True)
